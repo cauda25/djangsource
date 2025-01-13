bf3af7be0c035d9b3768d030705e4c5a5be1b6bd
ju <roronoa246@gmail.com>
2025-01-13 12:56:22 +0900

    form 변경경

--- myapp/todo/views.py
+++ myapp/todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Todo
+from .forms import *
 
 
 def todo_list(request):
@@ -22,22 +23,27 @@ def todo_detail(request, id):
 def todo_register(request):
     if request.method == "POST":
         # 폼 안의 내용 개별로 가져오기
-        title = request.POST.get("title")
-        description = request.POST.get("description")
-        important = request.POST.get("important")
+        # title = request.POST.get("title")
+        # description = request.POST.get("description")
+        # important = request.POST.get("important")
 
-        print(f"post {title},{description},{important}")
+        # print(f"post {title},{description},{important}")
 
-        if important:
-            todo = Todo(title=title, description=description, important=True)
-        else:
-            todo = Todo(title=title, description=description)
+        # if important:
+        #     todo = Todo(title=title, description=description, important=True)
+        # else:
+        #     todo = Todo(title=title, description=description)
 
-        todo.save()
-        return redirect("todo_list")
+        # todo.save()
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("todo_list")
 
     else:
-        return render(request, "todo/create.html")
+        form = TodoForm()
+
+    return render(request, "todo/create.html", {"form": form})
 
 
 def todo_edit(request, id):
@@ -45,31 +51,35 @@ def todo_edit(request, id):
 
     if request.method == "POST":
         # 폼 안의 내용 개별로 가져오기
-        title = request.POST.get("title")
-        description = request.POST.get("description")
-        completed = request.POST.get("completed")
-        important = request.POST.get("important")
+        # title = request.POST.get("title")
+        # description = request.POST.get("description")
+        # completed = request.POST.get("completed")
+        # important = request.POST.get("important")
 
-        print(f"post {title},{description},{completed},{important}")
+        # print(f"post {title},{description},{completed},{important}")
 
-        todo.title = title
-        todo.description = description
+        # todo.title = title
+        # todo.description = description
 
-        if important:
-            todo.important = True
-        else:
-            todo.important = False
+        # if important:
+        #     todo.important = True
+        # else:
+        #     todo.important = False
 
-        if completed:
-            todo.completed = True
-        else:
-            todo.completed = False
+        # if completed:
+        #     todo.completed = True
+        # else:
+        #     todo.completed = False
 
-        todo.save()
-        return redirect("todo_detail", id=todo.id)
+        # todo.save()
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect("todo_detail", id=todo.id)
 
     else:
-        return render(request, "todo/edit.html", {"todo": todo})
+        form = TodoForm(instance=todo)
+    return render(request, "todo/edit.html", {"form": form})
 
 
 def todo_done(request):
--- photoapp/photo/views.py
+++ photoapp/photo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Photo
+from .forms import *
 
 
 def photo_list(request):
@@ -9,25 +10,30 @@ def photo_list(request):
 
 def photo_post(request):
     if request.method == "POST":
-        title = request.POST.get("title")
-        author = request.POST.get("author")
-        image = request.POST.get("image")
-        description = request.POST.get("description")
-        prive = request.POST.get("prive")
+        # title = request.POST.get("title")
+        # author = request.POST.get("author")
+        # image = request.POST.get("image")
+        # description = request.POST.get("description")
+        # prive = request.POST.get("prive")
 
-        photo = Photo(
-            title=title,
-            author=author,
-            image=image,
-            description=description,
-            prive=prive,
-        )
+        # photo = Photo(
+        #     title=title,
+        #     author=author,
+        #     image=image,
+        #     description=description,
+        #     prive=prive,
+        # )
 
-        photo.save()
+        # photo.save()
 
-        return redirect("photo_detail", id=photo.id)
+        # form 사용
+        form = PhotoForm(request.POST)
+        if form.is_valid():
+            photo = form.save()
+            return redirect("photo_detail", id=photo.id)
     else:
-        return render(request, "photo/create.html")
+        form = PhotoForm()
+    return render(request, "photo/create.html", {"form": form})
 
 
 def photo_detail(request, id):
@@ -39,21 +45,27 @@ def photo_detail(request, id):
 def photo_edit(request, id):
     photo = Photo.objects.get(id=id)
     if request.method == "POST":
-        image = request.POST.get("image")
-        description = request.POST.get("description")
-        prive = request.POST.get("prive")
+        # image = request.POST.get("image")
+        # description = request.POST.get("description")
+        # prive = request.POST.get("prive")
 
-        if image:
-            photo.image = image
-        if description:
-            photo.description = description
-        if prive:
-            photo.prive = prive
+        # if image:
+        #     photo.image = image
+        # if description:
+        #     photo.description = description
+        # if prive:
+        #     photo.prive = prive
 
-        photo.save()
-        return redirect("photo_detail", id=id)
+        # photo.save()
+        form = PhotoForm(request.POST, instance=photo)
+        if form.is_valid():
+            form.save()
+            return redirect("photo_detail", id=id)
+
     else:
-        return render(request, "photo/edit.html", {"photo": photo})
+        form = PhotoForm(instance=photo)
+
+    return render(request, "photo/edit.html", {"form": form, "photo": photo})
 
 
 def photo_remove(request, id):
